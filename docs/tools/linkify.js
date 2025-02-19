6d9b3bdba00b01ed25f6ed70bb5b236db791ec9b
Addison Phillips <addison@unicode.org>
2025-02-19 07:10:45 -0800

    Clean up linkify.js
    - clean up messaging
    - check for terms that are not used
    - make the console messages make more sense
    - run beautifier

--- docs/tools/linkify.js
+++ docs/tools/linkify.js
@@ -1,39 +1,57 @@
-// Work in progress: tooling to linkify the HTML produced from
-// the MessageFormat 2 markdown.
-// this has been tested on the tr35-messageformat.html file
-// but not implemented in LDML45
+// Link terms (enclosed in <em>) to definitions (enclosed in <dfn>)
+
 function linkify() {
   const terms = findTerms();
   const missing = new Set();
+  const used = new Set();
   const links = document.querySelectorAll("em");
   links.forEach((item) => {
     const target = generateId(item.textContent);
     if (terms.has(target)) {
       const el = item.lastElementChild ?? item;
       el.innerHTML = `<a href="#${target}">${item.textContent}</a>`;
+      used.add(target);
     } else {
       missing.add(target);
     }
   });
   // report missing terms
+  // because the spec uses italics for other reasons (besides linking terms)
+  // not all of these are errors
   // (leave out sort if you want it in file order)
-  Array.from(missing).sort().forEach((item)=> {
+  if (missing.size > 0) {
+      console.log("Potentially missing definitions:");
+      Array.from(missing).sort().forEach((item)=> {
+          console.log(item);
+      });
+  }
+
+  if (terms.size === used.size) return;
+  console.log("Some definitions were not used:");
+  // report definitions that are never used in the specification
+  Array.from(terms).forEach(item)=> {
+      if (used.has(item)) continue;
       console.log(item);
-  });
+  }
 }
 
 function findTerms() {
   const terms = new Set();
+  var duplicateCount = 0;
   document.querySelectorAll("dfn").forEach((item) => {
     // console.log(index + ": " + item.textContent);
     const term = generateId(item.textContent);
-    // guard against duplicates
+    // warn about duplicate definitions in the spec
     if (terms.has(term)) {
-        console.log("Duplicate term: " + term);
+        console.log(term);
+        duplicateCount++;
     }
     terms.add(term);
     item.setAttribute("id", term);
   });
+  if (duplicateCount > 0) {
+      console.log("Duplicate Terms: " + duplicateCount);
+  }
   return terms;
 }
 
